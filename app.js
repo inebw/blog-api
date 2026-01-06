c2dc5c9f91ce3048d146fbd679703c590cf4fc8c
Punit Yadav <punit965@gmail.com>
2026-01-06 21:04:33 +0530

    added frontend info

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://blog-api-client-silk.vercel.app/',
     credentials:true
 }))
 
